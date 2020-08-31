3f508aacd3335889fa84ef9033d2bfc436d0cf2a
ARWAIMORISH <morisarwai@gmail.com>
2020-08-31 22:39:23 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const menu = document.querySelector(".menu");
+const nav = document.querySelector(".bottom");
+menu.addEventListener("click", () => {
+  if (nav.classList.contains("hidden")) {
+    nav.classList.remove("hidden");
+  } else {
+    nav.classList.add("hidden");
+  }
+});
